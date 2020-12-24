b4a28193afe17553e9f5b9f257cb0328a6881b0e
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-12-24 17:25:37 +0800

    Update 01_detect.py

--- 01_detect.py
+++ 01_detect.py
@@ -9,7 +9,7 @@ from numpy import random
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
-from utils.general01 import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
     strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
